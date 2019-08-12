8c89d5ec456aa41a087894b8839c42d7d29077b9
Siddhant Jajoo <sija6678@colorado.edu>
2019-08-12 12:43:13 -0600

    Assignment 4: Base Code

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -15,7 +15,7 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
-#TODO: Add required executables or scripts as required below
+#TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
 endef
