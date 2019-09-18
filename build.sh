0a02b2385197fc10110d14eeaff15172154d0b5b
Dan Walkes <danwalkes@trellis-logic.com>
2019-09-17 18:40:55 -0600

    Reorder prints, add external
    Use BR2_EXTERNAL in make commands for defconfig, this way the buildroot
    directory is setup to use the external directory from the first
    successful make.

--- build.sh
+++ build.sh
@@ -20,19 +20,19 @@ cd `dirname $0`
 if [ ! -e ${BUILDROOT}/.config ]
 then
 	echo -e "\n MISSING BUILDROOT CONFIGURATION FILE \n"
-	
+
 	if [ -e ${MODIFIED_DEFCONFIG} ]
 	then
 		echo "USING aesd_qemu_defconfig"
-		make -C ${BUILDROOT} defconfig BR2_DEFCONFIG=${MODIFIED_DEFCONFIG_REL_BUILDROOT}
+		make -C ${BUILDROOT} defconfig BR2_DEFCONFIG=${MODIFIED_DEFCONFIG_REL_BUILDROOT} BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
 	else
+		echo "Using default defconfig ${DEFAULT_DEFCONFIG}"
+		make -C ${BUILDROOT} defconfig BR2_DEFCONFIG=${DEFAULT_DEFCONFIG} BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
 		echo "Run ./save_config.sh to save this as the default configuration in ${MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
-		make -C ${BUILDROOT} defconfig BR2_DEFCONFIG=${DEFAULT_DEFCONFIG}
-	fi	
+	fi
 else
 	echo "\n USING EXISTING BUILDROOT CONFIG \n"
 	echo -e "\n TO FORCE UPDATE, DELETE .config OR MAKE CHANGES USING make menuconfig AND BUILD AGAIN\n"
 	make -C ${BUILDROOT} BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
-
 fi
