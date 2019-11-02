329883474fbecc61c423cd9b5ec744849d4ac2ec
steve <stxa1234@colorado.edu>
2019-11-01 21:20:15 -0600

    added qemu in config

--- shared.sh
+++ shared.sh
@@ -2,9 +2,12 @@
 # Shared definitions for buildroot scripts
 
 # The defconfig from the buildroot directory we use for qemu builds
-
+QEMU_DEFCONFIG=configs/qemu_aarch64_virt_defconfig
+# The default hardware platform, when building for hardware
 RPI_DEFCONFIG=configs/raspberrypi3_defconfig
 # The place we store customizations to the qemu configuration
+MODIFIED_QEMU_DEFCONFIG=base_external/configs/aesd_qemu_defconfig
+# The place we store customizations for the project current target
 MODIFIED_RPI_DEFCONFIG=base_external/configs/aesd_rpi_defconfig
 
 
