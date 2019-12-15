583a69081769ba0bc5d3931d4a6b9da7c5c4be06
AjiteshBatraCU <ajitesh.batra@colorado.edu>
2019-12-15 13:30:36 -0700

    Buildroot docker setup for running hello-world container

---
+++ base_external/external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_assignment_4_PATH)/package/*/*.mk))
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,13 +1,5 @@
-
-##############################################################
-#
-# AESD-ASSIGNMENTS
-#
-##############################################################
-
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_VERSION = '4ba7e0af770099aeba98e68f0b9501c38ef301d4'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-5013/Ajitesh-Abikamet-Final-Project-Application.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
 
@@ -17,8 +9,8 @@ endef
 
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/check-config.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/cgroupfs/cgroupfs-mount $(TARGET_DIR)/etc/init.d/S98cgroupfs-mount
 endef
-
 
 $(eval $(generic-package))
