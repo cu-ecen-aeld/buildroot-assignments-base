0d5a5c556041a7bea98bd2db7a6a1ad01f7d738a
Dan Walkes <daniel.walkes@colorado.edu>
2020-07-11 19:03:38 -0600

    Update aesd-assignments.mk
    Add note about specifying SITE

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,6 +7,9 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
