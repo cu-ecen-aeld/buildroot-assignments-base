cad5c59ae9eef4aeafe6062d84a735423118a192
Dan Walkes <danwalkes@trellis-logic.com>
2021-06-13 16:08:11 -0600

    Fix formattting on runqemu script

--- runqemu.sh
+++ runqemu.sh
@@ -4,4 +4,12 @@
 #Author: Siddhant Jajoo.
 
 
-qemu-system-aarch64 -M virt -cpu cortex-a53 -nographic -smp 1 -kernel buildroot/output/images/Image -append "rootwait root=/dev/vda console=ttyAMA0" -netdev user,id=eth0,hostfwd=tcp::10022-:22 -device virtio-net-device,netdev=eth0 -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
+qemu-system-aarch64 \
+    -M virt  \
+    -cpu cortex-a53 -nographic -smp 1 \
+    -kernel buildroot/output/images/Image \
+    -append "rootwait root=/dev/vda console=ttyAMA0" \
+    -netdev user,id=eth0,hostfwd=tcp::10022-:22 \
+    -device virtio-net-device,netdev=eth0 \
+    -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
+    -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
