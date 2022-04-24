c8ed3c3a8826eb4bfb5a46171c66d93fea6c6271
Hemanth Nandish <hena8085@colorado.edu>
2022-04-24 04:06:01 +0000

    fix file checks and indentation

--- runqemu-rpi.sh
+++ runqemu-rpi.sh
@@ -5,24 +5,30 @@ KERNEL="buildroot/output/images/Image"
 DTB="buildroot/output/images/bcm2710-rpi-3-b.dtb" 
 ROOTFS="buildroot/output/images/rootfs.ext2"
 
-if [ ! -f $KERNEL ]
+if [ ! -e $KERNEL ]
 then
 	echo "$KERNEL does not exist"
 	exit 1
 fi
 
-if [ ! -f $DTB ]
+if [ ! -e $DTB ]
 then
 	echo "$DTB does not exist"
 	exit 1
 fi
 
-if [ ! -f $ROOTFS ]
+if [ ! -e $ROOTFS ]
 then
 	echo "$ROOTFS does not exist"
 	exit 1
 fi
 
 
-qemu-system-aarch64 -M raspi3 -kernel $KERNEL -dtb $DTB -m 1024 -nographic -append "rw console=ttyAMA0,115200 root=/dev/mmcblk0 fsck.repair=yes rootwait" -device sd-card,drive=mycard -drive if=none,file=$ROOTFS,format=raw,id=mycard
+qemu-system-aarch64 \
+	-M raspi3 \
+	-kernel $KERNEL \
+	-dtb $DTB \
+	-m 1024 -nographic \
+	-append "rw console=ttyAMA0,115200 root=/dev/mmcblk0 fsck.repair=yes rootwait" \
+	-device sd-card,drive=mycard -drive if=none,file=$ROOTFS,format=raw,id=mycard
 
